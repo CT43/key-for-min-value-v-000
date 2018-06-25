b6d8a6fa3aee9a967bb73ee478bb37c2a662549a
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-25 22:16:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   new_hash = []
   name_hash.each do |key, value|
     if value < lowest
-      lowest == value
+      lowest = value
       new_hash = [key]
     end
   end
